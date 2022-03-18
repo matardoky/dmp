829467f14ae4a6d76ff5df78f8e2a7c92fdc77d2
MatarDoky <mdokydiop@gmail.com>
2022-03-18 13:21:08 +0100

    webpack antd django-react config

--- dmp/frontend/src/App.js
+++ dmp/frontend/src/App.js
@@ -1,16 +1,65 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { Link } from 'react-router-dom';
+import { enquireScreen } from 'enquire-js'
+
+
+import Navbar from './components/Home/Navbar';
+import { NavbarDataSource } from './components/Home/data.source';
 
 
 
-const App = () => {
-    return (
-        <div>
-            module patient 
-        </div>
-    );
+let isMobile
+enquireScreen((b) => {
+    isMobile = b
+})
+
+const { location = {}} = typeof window !== 'undefined' ? window : {}
+
+export default class App extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            isMobile,
+            show: !location.port
+        }
+    }
+
+    componentDidMount(){
+        enquireScreen((b)=>{
+           this.setState({ isMobile: !!b })
+        })
+
+        if(location.port){
+            setTimeout(()=>{
+                this.setState({
+                    show:true,
+                }),
+                500
+            })
+        }
+    }
+
+    render(){
+
+        const children = [
+            <Navbar 
+            id="nav"
+            key="nav"
+            isMobile={this.state.isMobile}
+            dataSource={NavbarDataSource}
+            />,
+        ]
+    
+        return (
+            <div className='template-wrapper'
+            ref={(d)=> {
+                this.dom = d
+            }}
+            >
+               {this.state.show && children}
+            </div>
+        );
+
+
+    }
 }
-
-export default App;
-
